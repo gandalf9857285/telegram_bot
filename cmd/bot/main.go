38870c9d7afd12aa196997cd4e68512c0d82b1a9
gandalf9857285 <sofia.burh@gmail.com>
2023-08-21 21:18:31 +0300

    21:18

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -7,28 +7,15 @@ import (
 )
 
 func main() {
-	bot, err := tgbotapi.NewBotAPI("")
+	bot, err := tgbotapi.NewBotAPI(token: "6586940301:AAGiotAJpM1BAEUJv4BmofG3kiH9i8nmMnE")
 	if err != nil {
 		log.Panic(err)
 	}
 
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-		}
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -1,1 +1,45 @@
 package telegram
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(bot *tgbotapi.BotAPI) *Bot {
+	return &Bot{bot: bot}
+}
+
+func (b *Bot) Start() error {
+	log.Printf("Authorized on account %s", b.bot.Self.UserName)
+
+	
+
+	b.handleUpdates(update)
+	return nil
+}
+
+func (b *Bot) handleUpdates(update tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			b.bot.Send(msg)
+		}
+	}
+}
+
+func (b *Bot) initUpdatesChannel() (tgbotapi.UpdatesChannel, error) {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		return err
+	}
+}
